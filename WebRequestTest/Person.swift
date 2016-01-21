a246343eeca893c259bf83f1aece7369cf1e6e71
Nicholas Smith <npsmith1990@gmail.com>
2016-01-21 13:12:06 -0600

    Create person class

---
+++ WebRequestTest/Person.swift
@@ -0,0 +1,23 @@
+//
+//  Person.swift
+//  WebRequestTest
+//
+//  Created by Nick on 1/21/16.
+//  Copyright © 2016 Nick. All rights reserved.
+//
+
+import Foundation
+
+class SWPerson {
+    private var _name: String!
+    private var _height: String!
+    private var _birthYear: String!
+    private var _hairColor: String!
+    
+    init(name: String, height: String, birthYear: String, hairColor: String) {
+        _name = name
+        _height = height
+        _birthYear = birthYear
+        _hairColor = hairColor
+    }
+}
